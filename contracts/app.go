b80732693143367084175e0c4caf1dbec02a85a9
Mohammad Julfikar <jxmxshakil@gmail.com>
2024-08-02 11:42:24 +0800

    Update services to include AppOpsContext parameter
    Refactored both IAppOneWayService and IAppBidirectionalService interfaces to include AppOpsContext as a parameter in their methods. This change aims to ensure that operations have access to context, messaging channels, and shutdown mechanisms, enhancing control and flexibility.

--- contracts/app.go
+++ contracts/app.go
@@ -1,18 +1,27 @@
 package contracts
 
+import "context"
+
+type AppOpsContext struct {
+	MsgChan  chan interface{}
+	Context  context.Context
+	Shutdown chan struct{}
+	Cancel   context.CancelFunc
+}
+
 type IAppOneWayService interface {
-	RetrieveUptoFuture() error
-	ActOnEvents() error
-	RetrieveOnlyUpdated() error
-	RetrieveSpecific(data interface{}) error
-	GetLogs() []interface{}
+	RetrieveUptoFuture(ctx AppOpsContext) error
+	ActOnEvents(ctx AppOpsContext) error
+	RetrieveOnlyUpdated(ctx AppOpsContext) error
+	RetrieveSpecific(ctx AppOpsContext, data interface{}) error
+	GetLogs(ctx AppOpsContext) []interface{}
 }
 
 type IAppBidirectionalService interface {
-	RetrieveUptoFuture() error
-	ActOnEvents() error
-	RetrieveOnlyUpdated() error
-	RetrieveSpecific(data interface{}) error
-	GetLogs() []interface{}
-	Sync() error
+	RetrieveUptoFuture(ctx AppOpsContext) error
+	ActOnEvents(ctx AppOpsContext) error
+	RetrieveOnlyUpdated(ctx AppOpsContext) error
+	RetrieveSpecific(ctx AppOpsContext, data interface{}) error
+	GetLogs(ctx AppOpsContext) []interface{}
+	Sync(ctx AppOpsContext) error
 }
