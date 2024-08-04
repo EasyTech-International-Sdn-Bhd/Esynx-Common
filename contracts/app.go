763147508d50cbbc3c6c53f04c58c0d901823ce4
Mohammad Julfikar <jxmxshakil@gmail.com>
2024-08-04 21:36:53 +0800

    Add dependencies and simplify service interfaces
    The `go.mod` and `go.sum` files were updated to include necessary dependencies. Additionally, service interfaces were refactored by removing context parameters and adding an Asynq task handler map to improve code simplicity and integration.

--- contracts/app.go
+++ contracts/app.go
@@ -1,27 +1,23 @@
 package contracts
 
-import "context"
-
-type AppOpsContext struct {
-	MsgChan  chan interface{}
-	Context  context.Context
-	Shutdown chan struct{}
-	Cancel   context.CancelFunc
-}
+import (
+	"context"
+	"github.com/hibiken/asynq"
+)
 
 type IAppOneWayService interface {
-	RetrieveUptoFuture(ctx AppOpsContext) error
-	ActOnEvents(ctx AppOpsContext) error
-	RetrieveOnlyUpdated(ctx AppOpsContext) error
-	RetrieveSpecific(ctx AppOpsContext, data interface{}) error
-	GetLogs(ctx AppOpsContext) []interface{}
+	RetrieveUptoFuture() error
+	ActOnEvents() error
+	RetrieveOnlyUpdated() error
+	RetrieveSpecific(data interface{}) error
+	GetHandlers() map[string]func(context.Context, *asynq.Task) error
 }
 
 type IAppBidirectionalService interface {
-	RetrieveUptoFuture(ctx AppOpsContext) error
-	ActOnEvents(ctx AppOpsContext) error
-	RetrieveOnlyUpdated(ctx AppOpsContext) error
-	RetrieveSpecific(ctx AppOpsContext, data interface{}) error
-	GetLogs(ctx AppOpsContext) []interface{}
-	Sync(ctx AppOpsContext) error
+	RetrieveUptoFuture() error
+	ActOnEvents() error
+	RetrieveOnlyUpdated() error
+	RetrieveSpecific(data interface{}) error
+	GetHandlers() map[string]func(context.Context, *asynq.Task) error
+	Sync() error
 }
