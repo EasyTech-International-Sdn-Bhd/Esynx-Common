b4f9fe68f685fd5500ce945713cf36ee4e5fdc57
Mohammad Julfikar <jxmxshakil@gmail.com>
2024-08-10 05:12:08 +0800

    Change itemPrice, quantity, and totalPrice to float64
    Switched data types of itemPrice, quantity, and totalPrice from string to float64 for better numerical operations. Added default values and maintained backward compatibility in the schema definition.

--- entities/cms_debitnote_details.go
+++ entities/cms_debitnote_details.go
@@ -7,10 +7,10 @@ type CmsDebitnoteDetails struct {
 	DnCode       string    `xorm:"VARCHAR(100)" json:"dnCode,omitempty" xml:"dnCode"`
 	ItemCode     string    `xorm:"VARCHAR(200)" json:"itemCode,omitempty" xml:"itemCode"`
 	ItemName     string    `xorm:"VARCHAR(200)" json:"itemName,omitempty" xml:"itemName"`
-	ItemPrice    string    `xorm:"VARCHAR(200)" json:"itemPrice,omitempty" xml:"itemPrice"`
-	Quantity     string    `xorm:"VARCHAR(200)" json:"quantity,omitempty" xml:"quantity"`
+	ItemPrice    float64   `xorm:"default 0 DOUBLE" json:"itemPrice,omitempty" xml:"itemPrice"`
+	Quantity     float64   `xorm:"default 0 DOUBLE" json:"quantity,omitempty" xml:"quantity"`
 	Uom          string    `xorm:"VARCHAR(200)" json:"uom,omitempty" xml:"uom"`
-	TotalPrice   string    `xorm:"VARCHAR(200)" json:"totalPrice,omitempty" xml:"totalPrice"`
+	TotalPrice   float64   `xorm:"default 0 DOUBLE" json:"totalPrice,omitempty" xml:"totalPrice"`
 	Discount     string    `xorm:"comment('0%+10+50%') VARCHAR(100)" json:"discount,omitempty" xml:"discount"`
 	ActiveStatus int       `xorm:"default 1 comment('0=inactive, 1=active') INT" json:"activeStatus,omitempty" xml:"activeStatus"`
 	UpdatedAt    time.Time `xorm:"not null default CURRENT_TIMESTAMP TIMESTAMP" json:"updatedAt,omitempty" xml:"updatedAt"`
