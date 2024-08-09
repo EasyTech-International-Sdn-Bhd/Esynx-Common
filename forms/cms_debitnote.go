6103c460f112541503841f6083d43e9393de53b1
Mohammad Julfikar <jxmxshakil@gmail.com>
2024-08-10 05:14:38 +0800

    Change item fields to use float64 for CmsDebitNoteDetailsForm
    Updated the data types of itemPrice, quantity, and totalPrice fields from string to float64. This change ensures accurate numerical representation and calculation, improving data integrity. Adjustments to the database schema were also made to reflect these new defaults.

--- forms/cms_debitnote.go
+++ forms/cms_debitnote.go
@@ -20,15 +20,15 @@ type CmsDebitNoteForm struct {
 }
 
 type CmsDebitNoteDetailsForm struct {
-	DnCode       string `xorm:"VARCHAR(100)" json:"dnCode,omitempty" xml:"dnCode"`
-	ItemCode     string `xorm:"VARCHAR(200)" json:"itemCode,omitempty" xml:"itemCode"`
-	ItemName     string `xorm:"VARCHAR(200)" json:"itemName,omitempty" xml:"itemName"`
-	ItemPrice    string `xorm:"VARCHAR(200)" json:"itemPrice,omitempty" xml:"itemPrice"`
-	Quantity     string `xorm:"VARCHAR(200)" json:"quantity,omitempty" xml:"quantity"`
-	Uom          string `xorm:"VARCHAR(200)" json:"uom,omitempty" xml:"uom"`
-	TotalPrice   string `xorm:"VARCHAR(200)" json:"totalPrice,omitempty" xml:"totalPrice"`
-	Discount     string `xorm:"comment('0%+10+50%') VARCHAR(100)" json:"discount,omitempty" xml:"discount"`
-	ActiveStatus int    `xorm:"default 1 comment('0=inactive, 1=active') INT" json:"activeStatus,omitempty" xml:"activeStatus"`
-	RefNo        string `xorm:"unique VARCHAR(200)" json:"refNo,omitempty" xml:"refNo"`
-	DnDtlUdf     string `xorm:"JSON" json:"dnDtlUdf,omitempty" xml:"dnDtlUdf"`
+	DnCode       string  `xorm:"VARCHAR(100)" json:"dnCode,omitempty" xml:"dnCode"`
+	ItemCode     string  `xorm:"VARCHAR(200)" json:"itemCode,omitempty" xml:"itemCode"`
+	ItemName     string  `xorm:"VARCHAR(200)" json:"itemName,omitempty" xml:"itemName"`
+	ItemPrice    float64 `xorm:"default 0 DOUBLE" json:"itemPrice,omitempty" xml:"itemPrice"`
+	Quantity     float64 `xorm:"default 0 DOUBLE" json:"quantity,omitempty" xml:"quantity"`
+	TotalPrice   float64 `xorm:"default 0 DOUBLE" json:"totalPrice,omitempty" xml:"totalPrice"`
+	Uom          string  `xorm:"VARCHAR(200)" json:"uom,omitempty" xml:"uom"`
+	Discount     string  `xorm:"comment('0%+10+50%') VARCHAR(100)" json:"discount,omitempty" xml:"discount"`
+	ActiveStatus int     `xorm:"default 1 comment('0=inactive, 1=active') INT" json:"activeStatus,omitempty" xml:"activeStatus"`
+	RefNo        string  `xorm:"unique VARCHAR(200)" json:"refNo,omitempty" xml:"refNo"`
+	DnDtlUdf     string  `xorm:"JSON" json:"dnDtlUdf,omitempty" xml:"dnDtlUdf"`
 }
