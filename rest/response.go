f0d40e82a0aae8a2ff5726e03d6940cfa0b7b216
Mohammad Julfikar <jxmxshakil@gmail.com>
2024-08-10 11:12:54 +0800

    Add response structs for various financial documents
    Introduced response structs for invoices, debit notes, and credit notes to facilitate structured API responses. This includes separate structs for sales and details of each document type, ensuring consistent and comprehensible data handling.

---
+++ rest/response.go
@@ -0,0 +1,57 @@
+package rest
+
+import "github.com/easytech-international-sdn-bhd/esynx-common/entities"
+
+type InvoiceResponse struct {
+	Data    []entities.CmsInvoice `json:"data"`
+	Status  bool                  `json:"status"`
+	Message string                `json:"message"`
+}
+
+type InvoiceSalesResponse struct {
+	Data    []entities.CmsInvoiceSales `json:"data"`
+	Status  bool                       `json:"status"`
+	Message string                     `json:"message"`
+}
+
+type InvoiceDetailsResponse struct {
+	Data    []entities.CmsInvoiceDetails `json:"data"`
+	Status  bool                         `json:"status"`
+	Message string                       `json:"message"`
+}
+
+type DebitNoteResponse struct {
+	Data    []entities.CmsDebitnote `json:"data"`
+	Status  bool                    `json:"status"`
+	Message string                  `json:"message"`
+}
+
+type DebitNoteSalesResponse struct {
+	Data    []entities.CmsDebitnoteSales `json:"data"`
+	Status  bool                         `json:"status"`
+	Message string                       `json:"message"`
+}
+
+type DebitNoteDetailsResponse struct {
+	Data    []entities.CmsDebitnoteDetails `json:"data"`
+	Status  bool                           `json:"status"`
+	Message string                         `json:"message"`
+}
+
+type CreditNoteResponse struct {
+	Data    []entities.CmsCreditnote `json:"data"`
+	Status  bool                     `json:"status"`
+	Message string                   `json:"message"`
+}
+
+type CreditNoteSalesResponse struct {
+	Data    []entities.CmsCreditnoteSales `json:"data"`
+	Status  bool                          `json:"status"`
+	Message string                        `json:"message"`
+}
+
+type CreditNoteDetailsResponse struct {
+	Data    []entities.CmsCreditnoteDetails `json:"data"`
+	Status  bool                            `json:"status"`
+	Message string                          `json:"message"`
+}
