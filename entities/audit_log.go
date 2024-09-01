5b7e1f2a083d6e54b0f4305f91e83e469f10665f
Mohammad Julfikar <jxmxshakil@gmail.com>
2024-09-01 17:50:11 +0800

    Add AuditLog entity
    This commit introduces the AuditLog entity, which tracks database operations like INSERT and UPDATE. It includes fields for operation type, record table, record ID, record content, user code, application name, and operation timestamp. This addition enables robust auditing and tracing of changes within the application.

---
+++ entities/audit_log.go
@@ -0,0 +1,17 @@
+package entities
+
+import "time"
+
+type AuditLog struct {
+	OperationType string    `xorm:"ENUM('INSERT', 'UPDATE') not null"`
+	RecordTable   string    `xorm:"VARCHAR(100)"`
+	RecordId      string    `xorm:"VARCHAR(80)"`
+	RecordBody    string    `xorm:"JSON"`
+	UserCode      string    `xorm:"VARCHAR(50) not null"`
+	AppName       string    `xorm:"VARCHAR(20) not null"`
+	OperationTime time.Time `xorm:"not null default CURRENT_TIMESTAMP TIMESTAMP"`
+}
+
+func (m *AuditLog) TableName() string {
+	return "audit_log"
+}
