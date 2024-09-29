4563c7860c988b11b44d66dd467bddc08043ebff
Mohammad Julfikar <jxmxshakil@gmail.com>
2024-09-29 21:43:52 +0800

    Add EDARoutes constants and modify GCP broker request handling
    Introduced a new file with EDARoutes constants to standardize route names. Updated GCP broker implementation to include the 'data' field directly in the request struct before JSON marshalling. Added the 'data' field to the request struct to support the updated broker logic.

--- eda/events/const.go
+++ eda/events/const.go
@@ -3,13 +3,23 @@ package events
 type EDARoutes string
 
 const (
-	EDARoute_Invoice           EDARoutes = "Invoice"
-	EDARoute_InvoiceDetails    EDARoutes = "InvoiceDetails"
-	EDARoute_CreditNote        EDARoutes = "CreditNote"
-	EDARoute_CreditNoteDetails EDARoutes = "CreditNoteDetails"
-	EDARoute_DebitNote         EDARoutes = "DebitNote"
-	EDARoute_DebitNoteDetails  EDARoutes = "DebitNoteDetails"
-	EDARoute_Debtor            EDARoutes = "Debtor"
-	EDARoute_DebtorBranch      EDARoutes = "DebtorBranch"
-	EDARoute_Agent             EDARoutes = "Agent"
+	EDARoute_Invoice    EDARoutes = "Invoice"
+	EDARoute_CreditNote EDARoutes = "CreditNote"
+	EDARoute_DebitNote  EDARoutes = "DebitNote"
+	EDARoute_Debtor     EDARoutes = "Debtor"
+	EDARoute_Agent      EDARoutes = "Agent"
+)
+
+type EDADataSource string
+
+const (
+	EDADataSource_Invoice           EDADataSource = "Invoice"
+	EDADataSource_InvoiceDetails    EDADataSource = "InvoiceDetails"
+	EDADataSource_DebitNote         EDADataSource = "DebitNote"
+	EDADataSource_DebitNoteDetails  EDADataSource = "DebitNoteDetails"
+	EDADataSource_CreditNote        EDADataSource = "CreditNote"
+	EDADataSource_CreditNoteDetails EDADataSource = "CreditNoteDetails"
+	EDADataSource_Debtor            EDADataSource = "Debtor"
+	EDADataSource_DebtorBranch      EDADataSource = "DebtorBranch"
+	EDADataSource_Agent             EDADataSource = "Agent"
 )
