7d80c21c4a0851c687e3f6ee0b7157febceace53
Mohammad Julfikar <jxmxshakil@gmail.com>
2024-10-01 22:12:05 +0800

    added back -- deleted some tables

--- entities/cms_module.go
+++ entities/cms_module.go
@@ -2,7 +2,7 @@ package entities
 
 type CmsModule struct {
 	ModuleId uint64 `xorm:"pk autoincr UNSIGNED BIGINT" json:"moduleId,omitempty" xml:"moduleId"`
-	Name     string `xorm:"not null unique 'id' VARCHAR(100)" json:"name,omitempty" xml:"name"`
+	Name     string `xorm:"not null unique 'idx' VARCHAR(100)" json:"name,omitempty" xml:"name"`
 	Value    []byte `xorm:"BLOB" json:"value,omitempty" xml:"value"`
 }
 
